9b1add9ad37b100fb18678fe1943847cecf7e3f7
Theresa Luu <theresa.luu@gmail.com>
2017-02-08 16:40:21 -0800

    test to ensure no weekend, no past, same day, 62+ bookings- need to address periodic valid factory test fail

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,37 @@
 class Reservation < ApplicationRecord
+  validates :party_size,
+    numericality: { only_integer: true , greater_than_or_equal_to: 1}
+  validates :children_u12,
+    numericality: { only_integer: true , greater_than_or_equal_to: 0}
+
+  %i(name email phone tour_date tour_time party_size children_u12)
+    .each do |a|
+      validates a, presence: true
+    end
+
+  validate :tour_date_cannot_be_out_of_range
+  validate :tour_date_cannot_be_today
+  validate :tour_date_cannot_be_weekend
+
+  private
+
+  def tour_date_cannot_be_out_of_range
+    if tour_date.present? && (tour_date > DateTime.now.beginning_of_day + 62.days)
+      errors.add(:tour_date, "tours cannot be booked this far in advance")
+    end
+  end
+
+  #if we are indeed keeping tour_date as DateTime, we have to keep ensure that
+  #tours can't be booked until the next day at opening hours
+  def tour_date_cannot_be_today
+    if tour_date.present? && (tour_date < (DateTime.now + 1.day).change(hour: 10))
+      errors.add(:tour_date, "tours cannot be booked for same day appointments")
+    end
+  end
+
+  def tour_date_cannot_be_weekend
+    if tour_date.present? && (tour_date.wday === 6  || tour_date.wday ===0)
+      errors.add(:tour_date, "tours cannot be booked for the weekend")
+    end
+  end
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,8 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  let(:reservation) {FactoryGirl.create(:reservation)}
+  let(:reservation) { FactoryGirl.create(:reservation) }
+  let(:res_90days) {
+    FactoryGirl.build(:reservation, tour_date: (DateTime.now + 90.days))
+  }
+
+  let(:res_today) {
+    FactoryGirl.build(:reservation, tour_date: (DateTime.now.noon))
+  }
+
+  let(:res_tomorrow_in_biz_hr) {
+    FactoryGirl.build(:reservation, tour_date: (DateTime.now + 1.day)
+                      .change(hour: 11))
+  }
+
+  let(:res_sat) {
+    additional_days = 6 - DateTime.now.beginning_of_day.wday
+    FactoryGirl.build(
+      :reservation, tour_date: (DateTime.now.beginning_of_day + additional_days.days)
+    )
+  }
+
+  let(:res_sun) {
+    additional_days = 7 - DateTime.now.beginning_of_day.wday
+    FactoryGirl.build(
+      :reservation, tour_date: (DateTime.now.beginning_of_day + additional_days.days)
+    )
+  }
+
   it "has a valid factory" do
     expect(reservation).to be_valid
   end
+
+  %i(name email phone tour_date tour_time party_size children_u12).each do |a|
+    it { should validate_presence_of(a) }
+  end
+
+  it "has at least on adult in each party" do
+    should validate_numericality_of(:party_size)
+      .only_integer
+      .is_greater_than_or_equal_to(1)
+  end
+
+  it "ensures that children_u12 is an integer" do
+    should validate_numericality_of(:children_u12)
+      .only_integer
+  end
+
+
+  it "allows tour dates to happen the next day up to 60 days out" do
+    expect(res_90days).not_to be_valid
+    expect(res_today).not_to be_valid
+    expect(res_tomorrow_in_biz_hr).to be_valid
+  end
+
+  it "prevents users from booking saturday tours" do
+    expect(res_sat).not_to be_valid
+  end
+
+  it "prevents users from booking sunday tours" do
+    expect(res_sun).not_to be_valid
+  end
+
 end
